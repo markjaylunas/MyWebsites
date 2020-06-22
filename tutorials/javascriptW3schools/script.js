cbc8301c019a09ee39a74c2d7cfd1571e598231d
markjaylunas <markjay.lunas@gmail.com>
2020-06-22 16:45:44 +0800

    My portfolio and professional website portfolio

---
+++ tutorials/javascriptW3schools/script.js
@@ -0,0 +1,7 @@
+function myFunction(code) {
+    if (code == 'change') {
+        document.getElementById("demo").innerHTML = "Paragraph changed.";
+    } else {
+        document.getElementById("demo").innerHTML = "";
+    }
+}
